620a1c105d580506168af31840cd269f101d4330
Almog Vagman Ciprut <almogvc@gmail.com>
2018-08-28 08:40:36 +0300

    refactor: Add transport base class

---
+++ src/models/transport.ts
@@ -0,0 +1,23 @@
+import { LogMessage } from './logMessage';
+import { TransportConfig } from './transportConfig';
+import { defaultTransportConfig } from '../transports/transportConfigs';
+
+export abstract class Transport {
+    config!: TransportConfig;
+
+    constructor(config?: TransportConfig) {
+        this.configure(config || defaultTransportConfig);
+    }
+
+    public configure(config: TransportConfig): void {
+        this.config = config;
+    }
+
+    public send(message: LogMessage, messageConfig?: any): void {
+        if (this.config.levels.includes(message.severity)) {
+            this.pass(this.config.format.getMessage(message), messageConfig);
+        }
+    }
+
+    public abstract pass(message: string, messageConfig?: any): void;
+}
