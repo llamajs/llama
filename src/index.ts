fcc17b5473e0e16bd1d92c314aee8ac1fa631c87
AlmogVC <almogvc@gmail.com>
2018-08-30 13:05:39 +0300

    refactor: Package ready for publish

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,23 @@
-import { Logger } from './logger/logger';
-import { ConsoleTransport } from './transports/console.transport';
-import { defaultSeverityLevels } from './severityLevels/severityLevels';
-import { LineFormat, JsonFormat, CsvFormat } from './formats/formats';
-import { LogMessage } from './messages/logMessage';
-import { LoggerConfig } from './logger/loggerConfig';
-import { Transport } from './transports/transport';
-import { RabbitMqTransport } from './transports/rabbitMQ/rabbitMQ.transport';
-import { RabbitMqConfig } from './transports/rabbitMQ/rabbitMQ.transport.config';
-import { RabbitMqMessageConfig } from './transports/rabbitMQ/rabbitMQ.message.config';
-import { FileTransportConfig } from './transports/file/file.transport.config';
-import { FileTransport } from './transports/file/file.transport';
+export * from './transports/transport';
+export * from './transports/transportConfig';
+export * from './transports/console.transport';
+export * from './transports/default.transport.config';
 
-const transports: Transport[] = [];
-transports.push(new ConsoleTransport({
-    levels: [defaultSeverityLevels.Info, defaultSeverityLevels.Error],
-    format: new LineFormat,
-}));
+export * from './transports/file/file.transport';
+export * from './transports/file/file.transport.config';
 
-transports.push(new RabbitMqTransport({
-    durable: false,
-    exchanegType: 'topic',
-    exchange: 'llamaLogs',
-    format: new JsonFormat,
-    host: 'localhost',
-    levels: [defaultSeverityLevels.Info, defaultSeverityLevels.Error],
-    password: 'guest',
-    persistent: false,
-    port: 15672,
-    username: 'guest',
+export * from './transports/rabbitMQ/rabbitMQ.transport.config';
+export * from './transports/rabbitMQ/rabbitMQ.message.config';
+export * from './transports/rabbitMQ/rabbitMQ.transport';
 
-} as RabbitMqConfig));
+export * from './severityLevels/severityLevels';
 
-transports.push(new FileTransport({
-    format: new CsvFormat({}),
-    levels: [defaultSeverityLevels.Info, defaultSeverityLevels.Error],
-    fileName: 'logs_csv_test.txt',
-    noNewLine: true,
-} as FileTransportConfig));
+export * from './messages/logMessage';
 
-transports.push(new FileTransport({
-    format: new JsonFormat({ rowDelimiter: ',' }),
-    levels: [defaultSeverityLevels.Info, defaultSeverityLevels.Error],
-    fileName: 'logs_json_test.txt',
-} as FileTransportConfig));
+export * from './logger/logger';
+export * from './logger/loggerConfig';
+export * from './logger/loggerConfigs';
 
-const loggerConfig: LoggerConfig = {
-    transports,
-    levels: (<any>Object).values(defaultSeverityLevels),
-};
-
-const logger: Logger = new Logger(loggerConfig);
-const message: LogMessage = {
-    severity: defaultSeverityLevels.Info,
-    name: 'test',
-    description: 'A not very helpful description',
-};
-
-const messageConfig: RabbitMqMessageConfig = {
-    routingKey: `${message.severity}.${message.name}`,
-};
-
-logger.log(message, messageConfig);
+export * from './formats/format';
+export * from './formats/format.config';
+export * from './formats/formats';
